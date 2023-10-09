83242538820f45490ece791696bb9b9da8d8c0eb
TheComit <killian.bertrand2002@gmail.com>
2023-10-09 15:44:36 +0200

    Class Team : test de création

--- src/main/java/fr/vivicoubar/minesteam/Team.java
+++ src/main/java/fr/vivicoubar/minesteam/Team.java
@@ -1,5 +1,21 @@
 package fr.vivicoubar.minesteam;
 
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
 public class Team {
+
+    String nom;
+    ArrayList<Player> playerList = new ArrayList<>();
+    Player leader;
+
+    public Team(String nom, Player leader) {
+        this.nom = nom;
+        this.leader = leader;
+        this.playerList.add(leader);
+    }
+
+
 
 }
