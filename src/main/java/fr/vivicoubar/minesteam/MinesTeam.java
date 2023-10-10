da5cc2b6ee9ea900ea55e04c60e9b38def4c11d2
TheComit <killian.bertrand2002@gmail.com>
2023-10-10 09:07:33 +0200

    Essai d'implémentation de commandes

--- src/main/java/fr/vivicoubar/minesteam/MinesTeam.java
+++ src/main/java/fr/vivicoubar/minesteam/MinesTeam.java
@@ -1,17 +1,131 @@
 package fr.vivicoubar.minesteam;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public final class MinesTeam extends JavaPlugin {
 
+    private HashMap<String, Team> playerTeams = new HashMap<>();
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        this.getCommand("teamcreate").setExecutor(this);
+        this.getCommand("inviteteam").setExecutor(this);
+        this.getCommand("teamaddsecond").setExecutor(this);
+        this.getCommand("teamremovesecond").setExecutor(this);
+        //loadTeams();
     }
 
     @Override
     public void onDisable() {
+        //saveTeams();
         // Plugin shutdown logic
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("teamcreate")) {
+            if (args.length != 1) { // Pas assez d'arguments rentrés
+                sender.sendMessage(ChatColor.RED + "Utilisation incorrecte. Utilisez /teamcreate <nom_de_l'équipe>");
+                return true;
+            }
+            String teamName = args[0];
+            Player leader = (Player) sender;
+            if (this.hasTeam(leader)) {
+                leader.sendMessage(ChatColor.RED + "Vous avez déjà une équipe ! Quittez la pour créer la votre.");
+                return true;
+            }
+
+            if (playerTeams.get(teamName) != null){
+                leader.sendMessage(ChatColor.RED + "Une team avec ce nom existe déjà ! Changez de nom.");
+                return true;
+            }
+
+            Team team = new Team(teamName, leader);
+            playerTeams.put(teamName, team);
+            team.addSecond(leader, leader); // Le chef est aussi un sous-chef
+            sender.sendMessage(ChatColor.GREEN + "L'équipe " + teamName + " a été créée avec succès. Le leader est " + leader.getName() + ".");
+            return true;
+        }
+        if (command.getName().equalsIgnoreCase("teamaddsecond")) {
+            if (args.length != 2) { // Pas assez d'arguments rentrés
+                sender.sendMessage(ChatColor.RED + "Utilisation incorrecte. Utilisez /teamaddsecond <nom_de_l'équipe> <nom_du_joueur>");
+                return true;
+            }
+            Player leader = (Player) sender;
+            Team team = playerTeams.get(args[0]);
+            Player player = Bukkit.getPlayer(args[1]);
+
+            if(team.getLeader() != leader) {
+                sender.sendMessage(ChatColor.RED + "Vous n'êtes pas le chef de cette équipe !");
+                return true;
+            }
+            team.addSecond(leader, player);
+        }
+        if (command.getName().equalsIgnoreCase("teamremovesecond")) {
+            if (args.length != 2) { // Pas assez d'arguments rentrés
+                sender.sendMessage(ChatColor.RED + "Utilisation incorrecte. Utilisez /teamremovesecond <nom_de_l'équipe> <nom_du_joueur>");
+                return true;
+            }
+            Player leader = (Player) sender;
+            Team team = playerTeams.get(args[0]);
+            Player player = Bukkit.getPlayer(args[1]);
+
+            if(team.getLeader() != leader) {
+                sender.sendMessage(ChatColor.RED + "Vous n'êtes pas le chef de cette équipe !");
+                return true;
+            }
+            team.removeSecond(leader, player);
+        }
+        if (command.getName().equalsIgnoreCase("teaminvite")) {
+            if (args.length < 2) { // Pas assez d'arguments rentrés
+                sender.sendMessage(ChatColor.RED + "Utilisation incorrecte. Utilisez /teaminvite <nom_de_l'équipe> <pseudo_du_joueur_à_inviter>");
+                return true;
+            }
+
+            String teamName = args[0];
+            Team team = playerTeams.get(teamName);
+            Player targetPlayer = Bukkit.getPlayer(args[1]);
+            Player senderPlayer = (Player) sender;
+
+            if (!team.getSecondList().contains(senderPlayer)) {
+                sender.sendMessage(ChatColor.RED + "Vous n'êtes pas au moins sous-chef de cette équipe, vous ne pouvez pas inviter quelqu'un !");
+                return true;
+            }
+            if (team.getPlayerList().contains(targetPlayer)){
+                sender.sendMessage(ChatColor.RED + "Le joueur spécifié est déjà dans l'équipe.");
+                return true;
+            }
+            if (targetPlayer == null || !targetPlayer.isOnline()){
+                sender.sendMessage(ChatColor.RED + "Le joueur spécifié n'est pas en ligne.");
+                return true;
+            }
+
+
+            //TODO : envoi d'une demande au joueur cible. Il doit accepter avec une commande (ou refuser)
+            //TODO : possibilité de supprimer une équipe (avec demande d'une confirmation)
+            //TODO : possibilité de quitter une équipe (avec demande d'une confirmation)
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean hasTeam(Player player) {
+        for (Map.Entry<String, Team> mapEntry : this.playerTeams.entrySet()) {
+            Team team = mapEntry.getValue();
+            if (team.getPlayerList().contains(player)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/fr/vivicoubar/minesteam/Team.java
+++ src/main/java/fr/vivicoubar/minesteam/Team.java
@@ -1,21 +1,74 @@
 package fr.vivicoubar.minesteam;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 
 public class Team {
 
-    String nom;
+    String name;
     ArrayList<Player> playerList = new ArrayList<>();
     Player leader;
+    ArrayList<Player> secondList = new ArrayList<>(); // Liste des seconds d'équipe
 
-    public Team(String nom, Player leader) {
-        this.nom = nom;
+    public Team(String name, Player leader) {
+        this.name = name;
         this.leader = leader;
         this.playerList.add(leader);
     }
 
+    public String getName(){
+        return name;
+    }
 
+    public Player getLeader() {
+        return leader;
+    }
 
+    public ArrayList<Player> getPlayerList(){
+        return playerList;
+    }
+
+    public ArrayList<Player> getSecondList() {
+        return secondList;
+    }
+
+    /**
+     * makes the player "player" a second of
+     *
+     * @param sender asking to put player as a second
+     * @param player going (or not) to be a second
+     */
+    public void addSecond(Player sender, Player player){
+        if(!this.playerList.contains(player)) {
+            sender.sendMessage(ChatColor.RED + player.getName() + " n'est pas dans l'équipe " + this.name +" ! Vous ne pouvez pas l'ajouter en tant que sous-chef...");
+            return;
+        }
+        if(!this.secondList.contains(player)) {
+            sender.sendMessage(ChatColor.GREEN + player.getName() + " a bien été ajouté en tant que sous-chef de l'équipe " + this.name +".");
+            this.secondList.add(player);
+            return;
+        }
+        sender.sendMessage(ChatColor.RED + player.getName() + " est déjà sous-chef de l'équipe " + this.name + " !");
+    }
+
+    /**
+     * removes the player "player" from the second list
+     *
+     * @param sender asking to remove
+     * @param player going (or not) to be a removed
+     */
+    public void removeSecond(Player sender, Player player){
+        if(!this.playerList.contains(player)) {
+            sender.sendMessage(ChatColor.RED + player.getName() + " n'est pas dans l'équipe !");
+            return;
+        }
+        if(this.secondList.contains(player)) {
+            sender.sendMessage(ChatColor.GREEN + player.getName() + " a bien été enlevé des sous-chefs de l'équipe " + this.name +".");
+            this.secondList.remove(player);
+            return;
+        }
+        sender.sendMessage(ChatColor.RED + player.getName() + " n'est pas sous-chef de l'équipe " + this.name + " !");
+    }
 }
