e0cf0497ed21f281476deecaeecde8da0cefc366
ViViCouBar <40126535+Vivicoubar@users.noreply.github.com>
2023-10-10 09:37:06 +0200

    Add Manager

---
+++ src/main/java/fr/vivicoubar/minesteam/TeamManager.java
@@ -0,0 +1,39 @@
+package fr.vivicoubar.minesteam;
+
+import java.util.ArrayList;
+import javax.annotation.Nullable;
+import org.bukkit.entity.Player;
+
+public class TeamManager {
+
+  static ArrayList<Team> teams;
+
+  public TeamManager(MinesTeam team) {
+    loadTeams();
+  }
+
+  public static ArrayList<Team> getTeams() {
+    return teams;
+  }
+
+  public static void loadTeams() {
+    //TODO CHARGER LES EQUIPES
+  }
+
+  @Nullable
+  public static Team getTeamByPlayer(Player toFindPlayer) {
+    for(Team team : teams) {
+      for (Player player : team.getPlayerList()) {
+        if (player.getUniqueId().equals(toFindPlayer.getUniqueId())) {
+          return team;
+        }
+      }
+    }
+    return null;
+  }
+
+  public static void addTeam(Team team) {
+    teams.add(team);
+  }
+
+}
